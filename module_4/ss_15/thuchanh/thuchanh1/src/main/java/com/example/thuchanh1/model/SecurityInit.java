55906366a78c7408d9a0a6537b7df66c6e9f423b
datnvn <idolsupperman3215@gmail.com>
2022-03-22 13:39:59 +0700

    hello kitty

---
+++ module_4/ss_15/thuchanh/thuchanh1/src/main/java/com/example/thuchanh1/model/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.example.thuchanh1.model;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ module_4/ss_15/thuchanh/thuchanh2/src/main/java/com/example/thuchanh2/service/JwtService.java
@@ -0,0 +1,10 @@
+package com.example.thuchanh2.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+
+}
