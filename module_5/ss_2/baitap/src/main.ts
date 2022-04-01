3ae473c3eac16c7b4b1358ac29d487c2747b72ba
datnvn <idolsupperman3215@gmail.com>
2022-04-01 15:07:02 +0700

    hello kitty

---
+++ module_5/ss_2/baitap/src/main.ts
@@ -0,0 +1,18 @@
+let fibonacci = (num: number): number => {
+    if (num <= 1) {
+        return num;
+    }else {
+        return fibonacci(num -1 + fibonacci(num - 2));
+    }
+}
+
+let sum: number = 0
+let printF = (num: number): void => {
+    for (let i = 1; i <= num; i++) {
+        console.log(`số fibonacci thứ ${i} là : `+fibonacci(i))
+        sum += fibonacci(i)
+    }
+    console.log(`tổng ${num} số fibonacci đầu tiên là :` + sum)
+}
+
+printF(10)
